903b54857eccce74e35aad5acee2744541ac12be
Muslimov-Xasan <muslimovxasan050@gmail.com>
2024-01-21 15:06:46 +0500

    ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Logo from "../../assets/Logo.svg";
 import Hero from "../../assets/Hero.png";
 import "./Header.scss";
+
 const Header = () => {
   const settings = {
     dots: true,
@@ -21,22 +22,19 @@ const Header = () => {
       <div className="wrapper__header">
         <div className="box">
           <img className="logo" src={Logo} alt="logo" width={128} />
-
           <input
             className="search"
             type="search"
             placeholder="Mahsulotlarni izlash"
           />
-
           <nav className="nav">
             <ul className="nav__list">
-          <button className="categoriya__btn">Kategoriya</button>
+              <button className="categoriya__btn">Kategoriya</button>
               <li className="nav__item">Kirish</li>
               <li className="nav__item">Qo’shish</li>
               <li className="nav__item">Sevimlilar</li>
             </ul>
           </nav>
-
           <select className="lang">
             <option value="ru">ruscha</option>
             <option value="kirilcha">kirilcha</option>
@@ -44,34 +42,15 @@ const Header = () => {
           </select>
         </div>
       </div>
-
       <Slider {...settings}>
         <div>
-          <img
-            className="silder-img"
-            src={Hero}
-            alt=""
-            width={1248}
-            height={450}
-          />
+          <img className="silder-img" src={Hero} alt="" width={1248} height={450} />
         </div>
         <div>
-          <img
-            className="silder-img"
-            src={Hero}
-            alt=""
-            width={1248}
-            height={450}
-          />
+          <img className="silder-img" src={Hero} alt="" width={1248} height={450} />
         </div>
         <div>
-          <img
-            className="silder-img"
-            src={Hero}
-            alt=""
-            width={1248}
-            height={450}
-          />
+          <img className="silder-img" src={Hero} alt="" width={1248} height={450} />
         </div>
       </Slider>
     </div>
